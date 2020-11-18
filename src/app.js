f0398465ced931af5dbdd2affdf84a98fc4c4e41
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-17 21:19:55 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 /// String de conexão
 
-mongoose.connect("mongodb+srv://simone:riqueza2007@cluster0.hgag9.mongodb.net/clientes?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://<password>@cluster0.hgag9.mongodb.net/<nomedobanco>?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
